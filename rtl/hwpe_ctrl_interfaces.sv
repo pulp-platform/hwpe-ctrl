49e380e102431c63422fc5b718f875cc1841a3fe
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2023-07-09 09:07:07 +0200

    Added req/rsp structures.

--- rtl/hwpe_ctrl_interfaces.sv
+++ rtl/hwpe_ctrl_interfaces.sv
@@ -22,70 +22,46 @@ interface hwpe_ctrl_intf_periph (
   input logic clk
 );
 
-  parameter int unsigned ID_WIDTH = -1;
+  parameter  int unsigned AddrWidth = -1;
+  parameter  int unsigned DataWidth = -1;
+  parameter  int unsigned ID_WIDTH  = -1;
+  localparam int unsigned BeWidth   = DataWidth/8;
 
-  logic                req;
-  logic                gnt;
-  logic [31:0]         add;
-  logic                wen;
-  logic [3:0]          be;
-  logic [31:0]         data;
-  logic [ID_WIDTH-1:0] id;
-  logic [31:0]         r_data;
-  logic                r_valid;
-  logic [ID_WIDTH-1:0] r_id;
+  logic                 req;
+  logic                 gnt;
+  logic [AddrWidth-1:0] add;
+  logic                 wen;
+  logic [BeWidth-1:0]   be;
+  logic [DataWidth-1:0] data;
+  logic [ID_WIDTH-1:0]  id;
+  logic [DataWidth-1:0] r_data;
+  logic                 r_valid;
+  logic [ID_WIDTH-1:0]  r_id;
 
   modport master (
-    output req, add, wen, be, data, id,
-    input  gnt, r_data, r_valid, r_id
+    output req,
+    output add,
+    output wen,
+    output be,
+    output data,
+    output id,
+    input  gnt,
+    input  r_data,
+    input  r_valid,
+    input  r_id
   );
   modport slave (
-    input  req, add, wen, be, data, id,
-    output gnt, r_data, r_valid, r_id
+    input  req,
+    input  add,
+    input  wen,
+    input  be,
+    input  data,
+    input  id,
+    output gnt,
+    output r_data,
+    output r_valid,
+    output r_id
   );
-
-`ifndef SYNTHESIS
-  task write(
-    input logic [31:0] w_add,
-    input logic [3:0]  w_be,
-    input logic [31:0] w_data,
-    input time TCP,
-    input time TA
-  );
-    #(TA);
-    add = w_add;
-    data = w_data;
-    wen = 1'b0;
-    req = 1'b1;
-    be = w_be;
-    id = '0;
-    while (gnt != 1'b1)
-      #(TCP);
-    #(TCP);
-    req = 1'b0;
-    #(TCP-TA);
-  endtask
-
-  task read(
-    input logic [31:0] r_add,
-    output logic [31:0] rdata,
-    input time TCP,
-    input time TA
-  );
-    #(TA);
-    add = r_add;
-    req = 1'b1;
-    wen = 1'b1;
-    id = '0;
-    while (gnt != 1'b1)
-      #(TCP);
-    #(TCP-TA);
-    rdata = r_data;
-    #(TA);
-    req = 1'b0;
-    #(TCP-TA);
-  endtask
-`endif
 
 endinterface // hwpe_ctrl_intf_periph
 
---
+++ rtl/include/hwpe-ctrl/typedef.svh
@@ -0,0 +1,28 @@
+// Copyright (c) 2020 ETH Zurich, University of Bologna
+//
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+`define HWPE_CTRL_TYPEDEF_REQ_T(req_t, addr_t, data_t, strb_t, id_t)\
+  typedef struct packed {                                           \
+    logic   req;                                                    \
+    logic   wen;                                                    \
+    strb_t  be;                                                     \
+    addr_t  add;                                                    \
+    data_t  data;                                                   \
+    id_t    id;                                                     \
+  } req_t;
+
+`define HWPE_CTRL_TYPEDEF_RSP_T(rsp_t, data_t, id_t)\
+  typedef struct packed {                           \
+    logic  gnt;                                     \
+    logic  r_valid;                                 \
+    data_t r_data;                                  \
+    id_t   r_id;                                    \
+  } rsp_t;
