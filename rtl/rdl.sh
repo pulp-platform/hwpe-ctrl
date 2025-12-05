280377c2f3b7c9708c65c6b3218af1be2b5f9ebe
Francesco Conti <f.conti@unibo.it>
2025-12-05 22:19:15 +0100

    make the example nature of hwpe_ctrl_regif_example more explicit!

--- rtl/rdl.sh
+++ rtl/rdl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-peakrdl regblock  hwpe_ctrl_regif.rdl -o rdl-example/ --cpuif obi-flat --default-reset arst_n --hwif-report
-peakrdl html      hwpe_ctrl_regif.rdl -o rdl-example/html/
-peakrdl c-header  hwpe_ctrl_regif.rdl -o rdl-example/hwpe_ctrl_target.h
+peakrdl regblock  hwpe_ctrl_regif_example.rdl -o rdl-example/ --cpuif obi-flat --default-reset arst_n --hwif-report
+peakrdl html      hwpe_ctrl_regif_example.rdl -o rdl-example/html/
+peakrdl c-header  hwpe_ctrl_regif_example.rdl -o rdl-example/hwpe_ctrl_target.h
 
