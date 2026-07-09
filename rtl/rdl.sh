6aaa836da19bc088ecb36f50c8087b9a61c43710
Francesco Conti <f.conti@unibo.it>
2026-07-09 23:03:02 +0200

    [target] move the PeakRDL-generated regif to use 'passthrough'
    This commit solves two issues:
     1) the previous usage of 'obi-flat' as interface for the register
        interface introduced one extra undesired cycle of latency.
     2) the 'swacc' bit used to compute soft_clear, commit, trigger
        signals is not synchronous to the 'value' bit.
    
    The first issue is solved by moving to 'passthrough', which
    reduces latency in the general register-write case (this is
    relevant for the main offload cases). The second issue is solved
    by delaying 'swacc' by 1 cycle: this introduces a cycle of delay
    but only to COMMIT_TRIGGER and SOFT_CLEAR, which is acceptable.

--- rtl/rdl.sh
+++ rtl/rdl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-peakrdl regblock  hwpe_ctrl_regif_example.rdl -o rdl-example/ --cpuif obi-flat --default-reset arst_n --hwif-report --addr-width 32
+peakrdl regblock  hwpe_ctrl_regif_example.rdl -o rdl-example/ --cpuif passthrough --default-reset arst_n --hwif-report --addr-width 32
 peakrdl html      hwpe_ctrl_regif_example.rdl -o rdl-example/html/
 peakrdl c-header  hwpe_ctrl_regif_example.rdl -o rdl-example/hwpe_ctrl_target.h
 # PeakRDL uses unpacked structs to avoid issues at compile time, which is commendable, but incompatible with FIFOing the output of the job!
